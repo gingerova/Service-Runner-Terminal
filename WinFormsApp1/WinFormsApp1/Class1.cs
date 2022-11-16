e5b2c75313a25be1fdfd4697081f79f3d9c01d0c
Hilal Türker <57413631+gingerova@users.noreply.github.com>
2022-11-16 11:47:54 +0300

    First Upload

---
+++ WinFormsApp1/WinFormsApp1/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    public class Class1
+
+    {
+        private SqlConnection con;
+        private SqlCommand comm;
+        string str = ConfigurationManager.AppSettings["CONNECTIONSTRING"];
+        string fp = ConfigurationManager.AppSettings["FILEPATH"];
+
+        public void runSP()
+        {
+            con = new SqlConnection(str);
+           
+            con.Open();
+            string dosya_yolu = @"" + fp + "";
+            FileStream fs = new FileStream(dosya_yolu, FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            DateTime dt = DateTime.Now;
+            sw.WriteLine(dt + "The service was ran.");
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+            con.Close();
+        }
+    }
+}
---
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,17 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class1 cs = new Class1();
+            cs.runSP();
+           
+    }
+    }
+}
